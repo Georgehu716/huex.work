160e9f2d81ce051f47d9b30b73c21e63fb6d47f6
Georgehu716 <georgehu716@qq.com>
2023-06-09 14:01:53 +0800

    Use templates system to render homepage

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def index(request):
-    return HttpResponse("<h1>Hello, this is huex.work</h1>")
+def home(request):
+    return render(request, "homepage/home.html")
